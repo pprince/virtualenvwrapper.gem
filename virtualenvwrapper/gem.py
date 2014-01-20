903ea584ec4ccd06f2c562d38c094b6e5459bd03
Paul Prince <paul@littlebluetech.com>
2014-01-19 22:23:58 -0500

    Add check for .gem inside venv.

--- virtualenvwrapper/gem.py
+++ virtualenvwrapper/gem.py
@@ -4,42 +4,42 @@ import os
 
 log = logging.getLogger(__name__)
 
+
 def post_activate_source(args):
     return """
-#patch to wrap gems inside the virtual env
+if [ -e "${VIRTUAL_ENV}/.gem"; then
+    if [ -n "${GEM_HOME+x}" ]; then
+        export _OLD_GEM_HOME="$GEM_HOME"
+    fi
+    export GEM_HOME=$VIRTUAL_ENV
 
-if [ -n "${GEM_HOME+x}" ]; then
-    export _OLD_GEM_HOME="$GEM_HOME"
+    if [ -n "${GEM_PATH+1}" ]; then
+        export _OLD_GEM_PATH="$GEM_PATH"
+    fi
+    export GEM_PATH=""
 fi
-export GEM_HOME=$VIRTUAL_ENV
-
-if [ -n "${GEM_PATH+1}" ]; then
-    export _OLD_GEM_PATH="$GEM_PATH"
-fi
-export GEM_PATH=""
 
 """
 
 
-
 def pre_deactivate_source(args):
     return  """
-#restore the value before exiting the venv
+if [ -e "${VIRTUAL_ENV}/.gem"; then
+    if [ -n "${_OLD_GEM_HOME+1}" ]; then
+        GEM_HOME="$_OLD_GEM_HOME"
+        export GEM_HOME
+        unset _OLD_GEM_HOME
+    else
+        unset GEM_HOME
+    fi
 
-if [ -n "${_OLD_GEM_HOME+1}" ]; then
-    GEM_HOME="$_OLD_GEM_HOME"
-    export GEM_HOME
-    unset _OLD_GEM_HOME
-else
-    unset GEM_HOME
-fi
-
-if [ -n "${_OLD_GEM_PATH+1}" ]; then
-    GEM_PATH="$_OLD_GEM_PATH"
-    export GEM_PATH
-    unset _OLD_GEM_PATH
-else
-    unset GEM_PATH
+    if [ -n "${_OLD_GEM_PATH+1}" ]; then
+        GEM_PATH="$_OLD_GEM_PATH"
+        export GEM_PATH
+        unset _OLD_GEM_PATH
+    else
+        unset GEM_PATH
+    fi
 fi
 
 """
