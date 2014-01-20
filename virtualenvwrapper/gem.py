b05e164e725f09a60c0b1c926ece26a66892b2b4
Paul Prince <paul@littlebluetech.com>
2014-01-19 21:31:05 -0500

    Commit the initial meat of my improvement.
    Differences in behavior from upstream version:
    
      * GEM_HOME will be set to VIRTUAL_ENV instead of
        VIRTUAL_ENV/lib/gems
    
          * As a handy side-effect of this improvement,
            we will no longer need to add anything to PATH.
    
      * Both the values, **and the set/unset** status, of
        all environmental variables will be preserved and
        restored properly on deactivating the virtualenv.

--- virtualenvwrapper/gem.py
+++ virtualenvwrapper/gem.py
@@ -7,12 +7,16 @@ log = logging.getLogger(__name__)
 def post_activate_source(args):
     return """
 #patch to wrap gems inside the virtual env
-export _OLD_GEM_HOME="$GEM_HOME"
-export _OLD_GEM_PATH="$GEM_PATH"
-export GEM_HOME="$VIRTUAL_ENV/lib/gems"
+
+if [ -n "${GEM_HOME+x}" ]; then
+    export _OLD_GEM_HOME="$GEM_HOME"
+fi
+export GEM_HOME=$VIRTUAL_ENV
+
+if [ -n "${GEM_PATH+1}" ]; then
+    export _OLD_GEM_PATH="$GEM_PATH"
+fi
 export GEM_PATH=""
-PATH="$GEM_HOME/bin:$PATH"
-export PATH
 
 """
 
@@ -21,7 +25,21 @@ export PATH
 def pre_deactivate_source(args):
     return  """
 #restore the value before entering the venv
-export GEM_HOME="$_OLD_GEM_HOME"
-export GEM_PATH="$GEM_PATH"
+
+if [ -n "${_OLD_GEM_HOME+1}" ]; then
+    GEM_HOME="$_OLD_GEM_HOME"
+    export GEM_HOME
+    unset _OLD_GEM_HOME
+else
+    unset GEM_HOME
+fi
+
+if [ -n "${_OLD_GEM_PATH+1}" ]; then
+    GEM_PATH="$_OLD_GEM_PATH"
+    export GEM_PATH
+    unset _OLD_GEM_PATH
+else
+    unset GEM_PATH
+fi
 
 """
