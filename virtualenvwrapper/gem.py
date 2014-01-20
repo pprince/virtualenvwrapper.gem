282d9ea779a5cef5a092651643761d999b5201b8
Paul Prince <paul@littlebluetech.com>
2014-01-20 02:14:11 -0500

    Squashed commit of the following:
    commit fc7e9683facff19763b4a8ac5eefaeee640f6467
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Mon Jan 20 02:04:01 2014 -0500
    
        Fix bugs.
    
    commit 8784f187056e5fd12c1f2b072c7fdc64399f8d43
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Sun Jan 19 22:31:37 2014 -0500
    
        Bump version.
    
    commit 66e41359fffe9697d935919041a3d1c61f5afac3
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Sun Jan 19 22:28:33 2014 -0500
    
        Add CONTRIBUTORS file.
    
    commit 903ea584ec4ccd06f2c562d38c094b6e5459bd03
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Sun Jan 19 22:23:58 2014 -0500
    
        Add check for .gem inside venv.
    
    commit 3dfc75443e83035d1b890e6d80f1c9b05c497071
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Sun Jan 19 21:40:31 2014 -0500
    
        Correct an incorrect comment.
    
    commit b05e164e725f09a60c0b1c926ece26a66892b2b4
    Author: Paul Prince <paul@littlebluetech.com>
    Date:   Sun Jan 19 21:31:05 2014 -0500
    
        Commit the initial meat of my improvement.
    
        Differences in behavior from upstream version:
    
          * GEM_HOME will be set to VIRTUAL_ENV instead of
            VIRTUAL_ENV/lib/gems
    
              * As a handy side-effect of this improvement,
                we will no longer need to add anything to PATH.
    
          * Both the values, **and the set/unset** status, of
            all environmental variables will be preserved and
            restored properly on deactivating the virtualenv.

--- virtualenvwrapper/gem.py
+++ virtualenvwrapper/gem.py
@@ -4,24 +4,42 @@ import os
 
 log = logging.getLogger(__name__)
 
+
 def post_activate_source(args):
     return """
-#patch to wrap gems inside the virtual env
-export _OLD_GEM_HOME="$GEM_HOME"
-export _OLD_GEM_PATH="$GEM_PATH"
-export GEM_HOME="$VIRTUAL_ENV/lib/gems"
-export GEM_PATH=""
-PATH="$GEM_HOME/bin:$PATH"
-export PATH
+if [ -e "${VIRTUAL_ENV}/.gem" ]; then
+    if [ -n "${GEM_HOME+x}" ]; then
+        export _OLD_GEM_HOME="$GEM_HOME"
+    fi
+    export GEM_HOME=$VIRTUAL_ENV
+
+    if [ -n "${GEM_PATH+x}" ]; then
+        export _OLD_GEM_PATH="$GEM_PATH"
+    fi
+    export GEM_PATH=""
+fi
 
 """
 
 
-
 def pre_deactivate_source(args):
     return  """
-#restore the value before entering the venv
-export GEM_HOME="$_OLD_GEM_HOME"
-export GEM_PATH="$GEM_PATH"
+if [ -e "${VIRTUAL_ENV}/.gem" ]; then
+    if [ -n "${_OLD_GEM_HOME+x}" ]; then
+        GEM_HOME="$_OLD_GEM_HOME"
+        export GEM_HOME
+        unset _OLD_GEM_HOME
+    else
+        unset GEM_HOME
+    fi
+
+    if [ -n "${_OLD_GEM_PATH+x}" ]; then
+        GEM_PATH="$_OLD_GEM_PATH"
+        export GEM_PATH
+        unset _OLD_GEM_PATH
+    else
+        unset GEM_PATH
+    fi
+fi
 
 """
