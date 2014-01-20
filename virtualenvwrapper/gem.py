fc7e9683facff19763b4a8ac5eefaeee640f6467
Paul Prince <paul@littlebluetech.com>
2014-01-20 02:04:01 -0500

    Fix bugs.

--- virtualenvwrapper/gem.py
+++ virtualenvwrapper/gem.py
@@ -7,13 +7,13 @@ log = logging.getLogger(__name__)
 
 def post_activate_source(args):
     return """
-if [ -e "${VIRTUAL_ENV}/.gem"; then
+if [ -e "${VIRTUAL_ENV}/.gem" ]; then
     if [ -n "${GEM_HOME+x}" ]; then
         export _OLD_GEM_HOME="$GEM_HOME"
     fi
     export GEM_HOME=$VIRTUAL_ENV
 
-    if [ -n "${GEM_PATH+1}" ]; then
+    if [ -n "${GEM_PATH+x}" ]; then
         export _OLD_GEM_PATH="$GEM_PATH"
     fi
     export GEM_PATH=""
@@ -24,8 +24,8 @@ fi
 
 def pre_deactivate_source(args):
     return  """
-if [ -e "${VIRTUAL_ENV}/.gem"; then
-    if [ -n "${_OLD_GEM_HOME+1}" ]; then
+if [ -e "${VIRTUAL_ENV}/.gem" ]; then
+    if [ -n "${_OLD_GEM_HOME+x}" ]; then
         GEM_HOME="$_OLD_GEM_HOME"
         export GEM_HOME
         unset _OLD_GEM_HOME
@@ -33,7 +33,7 @@ if [ -e "${VIRTUAL_ENV}/.gem"; then
         unset GEM_HOME
     fi
 
-    if [ -n "${_OLD_GEM_PATH+1}" ]; then
+    if [ -n "${_OLD_GEM_PATH+x}" ]; then
         GEM_PATH="$_OLD_GEM_PATH"
         export GEM_PATH
         unset _OLD_GEM_PATH
