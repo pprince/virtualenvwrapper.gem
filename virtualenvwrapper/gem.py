afe21e2619c397b6db762c2ea0f1c500f5f63462
Paul Prince <paul@littlebluetech.com>
2014-01-31 03:55:38 -0500

    Add Bundler support.

--- virtualenvwrapper/gem.py
+++ virtualenvwrapper/gem.py
@@ -8,6 +8,7 @@ log = logging.getLogger(__name__)
 def post_activate_source(args):
     return """
 if [ -e "${VIRTUAL_ENV}/.gem" ]; then
+
     if [ -n "${GEM_HOME+x}" ]; then
         export _OLD_GEM_HOME="$GEM_HOME"
     fi
@@ -17,6 +18,17 @@ if [ -e "${VIRTUAL_ENV}/.gem" ]; then
         export _OLD_GEM_PATH="$GEM_PATH"
     fi
     export GEM_PATH=""
+
+    if [ -n "${BUNDLE_HOME+x}" ]; then
+        export _OLD_BUNDLE_HOME="$BUNDLE_HOME"
+    fi
+    export BUNDLE_HOME="$GEM_HOME"
+
+    if [ -n "${BUNDLE_BIN+x}" ]; then
+        export _OLD_BUNDLE_BIN="$BUNDLE_BIN"
+    fi
+    export BUNDLE_BIN="${GEM_HOME}/bin"
+
 fi
 
 """
@@ -25,6 +37,7 @@ fi
 def pre_deactivate_source(args):
     return  """
 if [ -e "${VIRTUAL_ENV}/.gem" ]; then
+
     if [ -n "${_OLD_GEM_HOME+x}" ]; then
         GEM_HOME="$_OLD_GEM_HOME"
         export GEM_HOME
@@ -40,6 +53,23 @@ if [ -e "${VIRTUAL_ENV}/.gem" ]; then
     else
         unset GEM_PATH
     fi
+
+    if [ -n "${_OLD_BUNDLE_HOME+x}" ]; then
+        BUNDLE_HOME="$_OLD_BUNDLE_HOME"
+        export BUNDLE_HOME
+        unset _OLD_BUNDLE_HOME
+    else
+        unset BUNDLE_HOME
+    fi
+
+    if [ -n "${_OLD_BUNDLE_BIN+x}" ]; then
+        BUNDLE_BIN="$_OLD_BUNDLE_BIN"
+        export BUNDLE_BIN
+        unset _OLD_BUNDLE_BIN
+    else
+        unset BUNDLE_BIN
+    fi
+
 fi
 
 """
